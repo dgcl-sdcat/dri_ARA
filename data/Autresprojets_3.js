a23b4d86b7d39d4bb6a6b2d6dd336771d10c75b4
dgcl-sdcat <80058755+dgcl-sdcat@users.noreply.github.com>
2021-04-21 16:08:00 +0200

    Add files via upload

---
+++ data/Autresprojets_3.js
@@ -0,0 +1,8 @@
+var json_Autresprojets_3 = {
+"type": "FeatureCollection",
+"name": "Autresprojets_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Thème": "Autres", "Projet": "Raccordement de 14  lycées à des réseaux de chaleur vertueux ", "Coût": "1 500 000 €", "Subvention": "1 200 000 €" }, "geometry": { "type": "Point", "coordinates": [ 4.836954938, 45.74821059 ] } }
+]
+}
---
+++ data/ProjetsMobilittransportscollectifs_5.js
@@ -0,0 +1,9 @@
+var json_ProjetsMobilittransportscollectifs_5 = {
+"type": "FeatureCollection",
+"name": "ProjetsMobilittransportscollectifs_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Thème": "Mobilité – transports collectifs", "Projet": "Intitulé du projet : Projet « CFEL »\nAmélioration de l’accessibilité du bassin de mobilité autour de Pont de Chéruy Crémieu (Nord Isère) :\n- système de transport public en site propre pour la liaison Lyon – Meyzieu – Pont de Chéruy �", "Coût": "2 000 000 €", "Subvention": "1 600 000 €" }, "geometry": { "type": "Point", "coordinates": [ 5.171238384, 45.74922411 ] } },
+{ "type": "Feature", "properties": { "Thème": "Mobilité – transports collectifs", "Projet": "Ligne inter-urbaine de Bus à Haut Niveau de Service (BHNS) Trévoux-Lyon - créer une ligne de transport routier performante sur une voie majoritairement dédiée entre Trévoux (01) et Lyon (Métropole) via Sathonay Camp en réutilisant sur 18 km la vo", "Coût": "5 500 000 €", "Subvention": "4 400 000 €" }, "geometry": { "type": "Point", "coordinates": [ 4.776310398, 45.94060238 ] } }
+]
+}
